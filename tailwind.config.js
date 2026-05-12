1a841a55845c40887daf91ab30965dad1eb06122
boppthomas <thomas.bopp.pro@gmail.com>
2026-05-12 10:12:26 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } };
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        squidPink: "#ff2f7d",
+        squidGreen: "#16f2b3",
+        ink: "#070711",
+      },
+      boxShadow: {
+        glow: "0 0 50px rgba(255,47,125,.18)",
+      },
+    },
+  },
+  plugins: [],
+};
